c57aec7837a646e1d80da4520c7e7aad2b011478
Fredrik Lindroth <fredrik.lindroth@decerno.se>
2020-09-11 09:32:44 +0200

    frontend

--- CryptoAPI med Firebase/functions/index.js
+++ CryptoAPI med Firebase/functions/index.js
@@ -1,55 +1,60 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const express = require("express");
+const admin = require("firebase-admin");
+const cors = require("cors");
+
 admin.initializeApp();
 const db = admin.firestore();
 const app = express();
+app.use(cors({ origin: ['http://localhost:5000', 'https://crypto-api-90478.web.app'] }));
 
-app.get('/:id', async (request, response) => {
-    const userCollectionRef = db.collection('users');
-    const result = await userCollectionRef.doc(request.params.id).get();
+app.get("/:id", async (request, response) => {
+  const userCollectionRef = db.collection("users");
+  const result = await userCollectionRef.doc(request.params.id).get();
 
-    const id = result.id;
-    const user = result.data();
+  const id = result.id;
+  const user = result.data();
 
-    response.status(200).send( { id, ...user } );
-})
+  response.status(200).send({ id, ...user });
+});
 
-app.get('/', async (request, response) => {
-    const userCollectionRef = db.collection('users');
-    const result = await userCollectionRef.get();
+app.get("/", async (request, response) => {
+  const userCollectionRef = db.collection("users");
+  const result = await userCollectionRef.get();
 
-    let users = [];
-    result.forEach((userDoc) => {
-        const id = userDoc.id;
-        const data = userDoc.data();
-        users.push({ id, ...data });
-    })
+  let users = [];
+  result.forEach((userDoc) => {
+    const id = userDoc.id;
+    const data = userDoc.data();
+    users.push({ id, ...data });
+  });
 
-    response.status(200).send(users);
-})
+  response.status(200).send(users);
+});
 
-app.put('/:id', async (request, response) => {
-    const userCollectionRef = db.collection('users');
-    const result = await userCollectionRef.doc(request.params.id).update(request.body);
+app.put("/:id", async (request, response) => {
+  const userCollectionRef = db.collection("users");
+  const result = await userCollectionRef
+    .doc(request.params.id)
+    .update(request.body);
 
-    response.status(200).send(result);
-})
+  response.status(200).send(result);
+});
 
-app.post('/', async function(request, response) {
-    const newUser = request.body;
-    const userCollectionRef = db.collection('users');
-    const result = await userCollectionRef.add(newUser);
+app.post("/", async (request, response) => {
+  const newUser = JSON.parse(request.body);
+  const userCollectionRef = db.collection("users");
+  const result = await userCollectionRef.add(newUser);
 
-    response.status(200).send(result);
-})
+  response.status(200).send(result);
+});
 
-app.delete('/:id', async function(request, response) {
-    const userId = request.params.id;
-    const userCollectionRef = db.collection('users');
-    const result = await userCollectionRef.doc(userId).delete();
+app.delete("/:id", async (request, response) => {
+  const userId = request.params.id;
+  const userCollectionRef = db.collection("users");
+  const result = await userCollectionRef.doc(userId).delete();
 
-    response.status(200).send(result);
-})
+  response.status(200).send(result);
+});
 
 exports.users = functions.https.onRequest(app);
---
+++ CryptoAPI med Firebase/public/index.js
@@ -0,0 +1,123 @@
+// const url = 'http://localhost:5001/crypto-api-90478/us-central1/users/';
+const url = 'https://us-central1-crypto-api-90478.cloudfunctions.net/users/';
+
+let users = [];
+let selectedUser = {};
+
+const tableSection = document.querySelector("#tableSection");
+const editSection = document.querySelector("#editSection");
+const userTable = document.querySelector("#userTable");
+const createUserButton = document.querySelector("#createUserButton");
+const userNameField = document.querySelector("#userNameField");
+const userEmailField = document.querySelector("#userEmailField");
+
+
+const initialize = () => {
+    getUsers();
+    createUserButton.addEventListener("click", createUser);
+    deleteUserButton.addEventListener("click", deleteUser);
+}
+ 
+const showEditSection = (userId) => {
+    if(!userId) {
+        throw new Error("No id present.");
+    }
+
+    selectedUser = userId;
+    tableSection.style.display = "none";
+    editSection.style.display = "block";
+}
+
+const showTableSection = () => {
+    tableSection.style.display = "block";
+    editSection.style.display = "none";
+}
+
+const getUsers = async () => {
+    try {
+        const response = await fetch(url);
+        
+        if(response.ok) {
+            users = await response.json();
+            renderTable();
+        }
+        else {
+            throw new Error(response.statusText);
+        }
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+const createUser = async () => {
+
+    const newUser = { name: userNameField.value, email: userEmailField.value };
+    if(!newUser.name || !newUser.email) {
+        console.log("TODO: Validering");
+        return;
+    }
+    
+    try {
+        const response = await fetch(url, { method: "POST", body: JSON.stringify(newUser) });
+        
+        if(response.ok) {
+            // if everything went ok we add the new user to the table, rerender the table and clears the form.
+            users.push(newUser);
+            renderTable();
+            clearForm();
+        }
+        else {
+            throw new Error(response.statusText);
+        }
+    } 
+    catch (err) {
+        throw err;
+    }
+}
+
+const deleteUser = async () => {
+
+    if(!selectedUser) {
+        throw new Error("No user id found.");
+    }
+    
+    try {
+        const response = await fetch(url + selectedUser, { method: "DELETE" });
+        
+        if(response.ok) {
+            // if everything went ok we filter out the user that was removed.
+            users = users.filter(user => user.id !== selectedUser);
+            renderTable();
+            showTableSection();
+        }
+        else {
+            throw new Error(response.statusText);
+        }
+    } 
+    catch (err) {
+        throw err;
+    }
+}
+
+const renderTable = () => {
+    let tableRow = "";
+
+    users.forEach(user => {
+        console.log(user);
+        tableRow += 
+        `<tr id=${user.id} onclick="showEditSection(this.id)">
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+        </tr>`;
+    });
+
+    userTable.innerHTML = tableRow;
+}
+
+const clearForm = () => {
+    userNameField.value = '';
+    userEmailField.value = '';
+}
+
+initialize();
